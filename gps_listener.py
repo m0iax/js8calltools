5ed68b99448453402150990fa1649ce36264a0b4
Mark M0IAX <m0iax@outlook.com>
2019-05-24 15:43:07 +0100

    Initial commit

---
+++ gps_listener.py
@@ -0,0 +1,91 @@
+'''
+Created on 21 May 2019
+JS8CallGPSUI Copyright 2019 M0IAX
+@author: Mark Bumstead M0IAX
+http://m0iax.com
+'''
+
+ 
+import threading
+from gps import *
+import time
+import os
+import maidenhead as mh
+import sys
+
+exitFlag=False
+
+def setexit(flag):
+    exitFlag=flag
+    
+class GpsListener(threading.Thread):
+
+   def __init__(self):
+       threading.Thread.__init__(self)
+       
+       self.current_lon = None
+       self.current_lat = None
+       self.current_gpstime = None
+       self.current_gpstime =  None
+       self.current_mhgrid = "No Fix"
+       self.current_latlon = None
+       self.runFlag=True
+       self.enabled=False
+       
+       self.session = gps(mode=WATCH_ENABLE)
+       
+   def set_enabled(self,flag):
+       self.enabled=flag
+   def get_enabled(self):
+       return self.enabled
+   def set_exitFlag(self,flag):
+       self.exitflag=flag
+   def get_current_lon(self):
+       return self.current_lon
+   def get_current_lat(self):
+       return self.current_lat
+   def get_current_latlon(self):
+       self.current_latlon = self.current_lat, self.current_lon
+       return self.current_latlon
+   def get_current_gpstime(self):
+       return self.current_gpstime
+   def get_current_mhgrid(self):
+       return self.current_mhgrid
+   def setrun(self,flag):
+       print("Shutting down gps listener. Please wait...")
+       self.runFlag=flag
+       
+
+   def run(self):
+       try:
+            while self.runFlag:
+
+                data = self.session.next()
+                if data['class'] == 'TPV':
+                    lat= getattr(data,'lat',0.0)
+                    lon = getattr(data, 'lon', 0.0)
+#            
+                    gpstime = getattr(data,'time', 0)
+#            
+                    grid = mh.toMaiden(lat, lon, 4)
+                    
+                    self.current_lat = lat
+                    self.current_lon = lon
+                    self.current_gpstime = gpstime
+                    self.current_mhgrid = grid
+                    currentMHGrid = grid
+                
+                time.sleep(1)
+                
+                
+                
+       except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+            print ("\nKilling Thread...")
+            self.runFlag = False
+            self.join() # wait for the thread to finish what it's doing
+            print ("Done.\nExiting.")
+
+gpsl = GpsListener()
+print("Starting GPS Listener")
+gpsl.start()
+
---
+++ readme.txt
@@ -0,0 +1,41 @@
+
+JSCall GPS Tools
+By Mark, M0IAX
+
+This software is provided as-is with no warranty whatsoever. Use it at your own risk.
+
+Introduction
+
+This utility will allow you to get your current maidenhead locator from GPSD and set JS8Call's locator.
+You may then use this app to send the ALLCALL command in js8call.
+
+First install the pre-requisites:
+
+Open a terminal window and enter the following commands:
+
+sudo apt-get install python3
+
+pip3 install gps
+pip3 install maidenhead
+
+
+Download the zip file
+
+wget http://m0iax.com/downloads/js8calltools.zip
+
+Next Unzip the files to your home directory
+
+Command line
+
+unzip js8calltoolz.zip
+
+In the command line type
+
+cd ~/js8calltools
+
+chmod +x js8callgpsUI.py
+
+Finally to run the software enter this command:
+
+./js8callgpsUI.py
+

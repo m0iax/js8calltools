2a66c4ffc2f10b16e59a3d8bb1be9f33313aa4dc
Mark M0IAX <m0iax@outlook.com>
2019-06-04 19:20:34 +0100

    added new tool stubs
    placeholders for new tools

---
+++ JS8CallInboxViewer.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+'''
+Created on 4 June 2019
+JS8CallInboxViewer by M0IAX
+@author: Mark Bumstead M0IAX
+http://m0iax.com
+'''
+
+import tkinter as tk
+from tkinter import StringVar
+import socket, time, json
+
+
